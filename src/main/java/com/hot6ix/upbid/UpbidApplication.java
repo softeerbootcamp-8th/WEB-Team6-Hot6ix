70192eafb5a7b01f2e9f4a4609230d6eeaaf77d7
choiseoji <seoji0538@naver.com>
2026-07-21 17:44:33 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/hot6ix/upbid/UpbidApplication.java
@@ -0,0 +1,13 @@
+package com.hot6ix.upbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hot6ix/upbid/UpbidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hot6ix.upbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpbidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
