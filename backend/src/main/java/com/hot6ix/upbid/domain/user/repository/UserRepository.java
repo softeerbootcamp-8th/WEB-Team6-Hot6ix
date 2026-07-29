34ac6723a4ccbf49cbb1264a69562edbe0e6d427
choiseoji <choiseoji123@naver.com>
2026-07-30 02:59:05 +0900

    [BE] refactor: domain.auth.client 중복 패키지 제거

--- backend/src/main/java/com/hot6ix/upbid/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/hot6ix/upbid/domain/user/repository/UserRepository.java
@@ -7,11 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByProviderId(String providerId);
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByUserIdAndDeletedAtIsNull(Long userId);
+
+    Optional<User> findByProviderId(String providerId);
 }
