37fe56ce2b53b22ed482ae9aed19e0719b33157c
choiseoji <choiseoji123@naver.com>
2026-08-02 18:22:49 +0900

    [BE] fix: SSE 구독 엔드포인트에 @GuestAllowed 추가
    비로그인 사용자도 경매방 SSE 구독이 가능하도록 인증 예외 처리.

--- backend/src/main/java/com/hot6ix/upbid/domain/sse/controller/SseController.java
+++ backend/src/main/java/com/hot6ix/upbid/domain/sse/controller/SseController.java
@@ -2,6 +2,7 @@ package com.hot6ix.upbid.domain.sse.controller;
 
 import com.hot6ix.upbid.domain.sse.api.SseApi;
 import com.hot6ix.upbid.domain.sse.service.SseService;
+import com.hot6ix.upbid.global.interceptor.GuestAllowed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class SseController implements SseApi {
     private final SseService sseService;
 
+    @GuestAllowed
     @RequestMapping(value = "/auction-rooms/{roomId}/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@PathVariable Long roomId){
         return sseService.subscribe(roomId);
